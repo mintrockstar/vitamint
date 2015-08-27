f188218129f9f807803381a4949cdfbc39e25391
mintrockstar <h.pongsathorn@gmail.com>
2015-08-27 17:06:15 +0700

    Rev1
    Rev1

---
+++ Bootstrap_test.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> Bootstrap Test </title>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+
+	
+       <script type="text/javascript">
+            $(function(){
+                 $('.btn').click(function(){
+                $('#myModal').modal();
+                });
+            });
+        </script>
+
+</head>	
+
+<body>
+	<h1>Hello World< 12334556788/h1>
+
+	
+<ul class="nav nav-pills">
+    <li class="active">
+        <a href="#">Home</a>
+    </li>
+    <li>
+        <a href="https://www.google.com">Profile</a>
+    </li>
+    <li>
+        <a href="#">Messages</a>
+    </li>
+</ul>
+
+<table class="table table-bordered table-hover table-condensed">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+    </tbody>
+</table>
+
+    </div>
+
+
+<a href="#" class="btn">Launch modal</a>
+
+<div id="myModal" class="modal hide">
+    <p>Bootstrap tutorials by Siam HTML</p>
+</div>
+
+
+
+	<script src="bootstrap/js/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+
+</html>
